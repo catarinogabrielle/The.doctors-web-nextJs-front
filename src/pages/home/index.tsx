2d14a4842da9d454e6f1d496d16ce39051bb6b40
Gabrielle <gabrielle.castro@ymonetize.com>
2023-02-15 02:27:56 -0300

    Atualização sobre

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -10,6 +10,7 @@ import { Header } from '../../components/Header'
 import { AuthContext } from '../../contexts/AuthContext'
 
 import { FiUser, FiBook, FiFile, FiBox, FiVideo } from 'react-icons/fi'
+import { AiOutlineInfoCircle } from 'react-icons/ai'
 
 export default function Home() {
     const { user } = useContext(AuthContext)
@@ -63,6 +64,10 @@ export default function Home() {
                             </button>
                         </Link>
                         */}
+                        <a href="https://thedoctorsagencia.com.br/" target="_blank" rel="noopener noreferrer" className={styles.boxMenu}>
+                            <AiOutlineInfoCircle className={styles.iconMenu} size={22} />
+                            <p>Quem Somos</p>
+                        </a>
                     </div>
                 ) : (
                     <div className={styles.contentMenulLoader}>
