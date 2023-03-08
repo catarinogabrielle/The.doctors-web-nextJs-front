12dd525d9eb960dc94ec1bdf7d6c92f8560203a2
Gabrielle <gabrielle.castro@ymonetize.com>
2023-03-08 00:50:14 -0300

    atualização

--- src/pages/api/sendEmail.ts
+++ src/pages/api/sendEmail.ts
@@ -1,12 +1,10 @@
 const nodemailer = require("nodemailer")
 const sendgridTransport = require("nodemailer-sendgrid-transport")
 
-const apiKey = process.env.API_KEY_SEND_GRID
-
 const transporter = nodemailer.createTransport(
     sendgridTransport({
         auth: {
-            api_key: apiKey
+            api_key: process.env.API_KEY_SEND_GRID
         }
     })
 )
@@ -17,11 +15,11 @@ export default async (req, res) => {
     try {
 
         const message = {
-            from: 'catarinogabrielle@gmail.com',
+            from: 'gabrielle.castro@ymonetize.com',
             to: email,
             subject: `Link de pagamento do curso ${courseName}`,
             html: `<p>Olá ${name}</p><b>O curso ${courseName} já é quase seu.</b><br><br><text>Acesse o link ${paymentlink} para efetuar o pagamento, e iniciar o curso.</text><br><br><text>Após o pagamento ser confirmado, você podera acessar o curso no período de 1 dia</text><br><text>Em caso de dúvidas, acesse nossa central de ajuda ou envie um e-mail para catarinogabrielle@gmail.com .</text><br><p><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Consulte nossa equipe para obter mais informações sobre a plataforma. <p><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Este e-mail foi enviado para: </font></font><a href="mailto:${email}" target="_blank"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">catarinogabrielle@gmail.com</font></font></a><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"> . </font><font style="vertical-align: inherit;">Não responda a este e-mail, uma vez que este endereço de e-mail não é monitorado.</font></font></p><p><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">© 2023 The doctors agência e suas entidades relacionadas. </font><font style="vertical-align: inherit;">Todos os direitos reservados.</font></font></p>`,
-            replyTo: 'catarinogabrielle@gmail.com'
+            replyTo: 'gabrielle.castro@ymonetize.com'
         }
 
         await transporter.sendMail(message)
